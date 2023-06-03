a90fc93a48e241671cea0d0f0fd9bd67cb452151
nbaciyarkin <kbyarkin@gmail.com>
2023-06-03 10:34:03 +0300

    Initial Commit

---
+++ GeneralElectricCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeneralElectricCalculator
+//
+//  Created by Yarkın Gazibaba on 3.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
